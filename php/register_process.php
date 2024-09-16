e4862dbc0f02a5f2b2a4e1c7562ab5209f8341c1
Wavezy11 <wavezy.dd@gmail.com>
2024-09-16 11:04:28 +0200

    updated css

--- php/register_process.php
+++ php/register_process.php
@@ -36,10 +36,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register Process</title>
-    <style></style>
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <style>
+   
+    </style>
 </head>
 <body>
+    <header>
+        <div class="header-content">
+            <h1>Welkom</h1>
+        </div>
+    </header>
+
+    <main>
     
+    </main>
 </body>
 </html>
